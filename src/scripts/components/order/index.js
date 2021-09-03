d30f24e62381ae9f2e3b81b703095c350c86b164
Toton Dwi Antoko <Totonantoko@gmail.com>
2021-09-03 09:10:51 +0700

    implement hook in quantity add and minus

--- src/scripts/components/order/index.js
+++ src/scripts/components/order/index.js
@@ -1,32 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './order.scss';
 
 const ItemOrder = ({
-  orderTitle, orderDescription, linkImg, price, quantity,
-}) => (
-  <div className="item-order">
-    <div className="info-order">
-      <img src={linkImg} alt="Order" />
-      <div className="text">
-        <h1>{orderTitle}</h1>
-        <p>{orderDescription}</p>
+  orderTitle, orderDescription, linkImg, price,
+}) => {
+  const [quantity, setQuantity] = useState(0);
+
+  return (
+    <div className="item-order">
+      <div className="info-order">
+        <img src={linkImg} alt="Order" />
+        <div className="text">
+          <h1>{orderTitle}</h1>
+          <p>{orderDescription}</p>
+        </div>
       </div>
+      <p className="price">
+        $
+        {price}
+      </p>
+      <div className="quantity">
+        <button type="submit" onClick={() => setQuantity(quantity - 1)}>-</button>
+        <p className="">{quantity}</p>
+        <button type="submit" onClick={() => setQuantity(quantity + 1)}>+</button>
+      </div>
+      <p className="total-price">
+        $
+        {quantity * price}
+      </p>
     </div>
-    <p className="price">
-      $
-      {price}
-    </p>
-    <div className="quantity">
-      <button type="submit">-</button>
-      <p className="">{quantity}</p>
-      <button type="submit">+</button>
-    </div>
-    <p className="total-price">
-      $
-      {quantity * price}
-    </p>
-  </div>
-);
+  );
+};
 const Order = () => (
   <div className="order">
     <div className="breadcrumb">
@@ -50,21 +54,18 @@ const Order = () => (
           orderDescription="Viverra fames massa integer fermentum nisl. Neque morbi cond"
           linkImg="assets/order/order-1.png"
           price="20"
-          quantity="2"
         />
         <ItemOrder
           orderTitle="Roberto graduation Gift"
           orderDescription="Viverra fames massa integer fermentum nisl. Neque morbi cond"
           linkImg="assets/order/order-2.png"
           price="20"
-          quantity="2"
         />
         <ItemOrder
           orderTitle="Roberto graduation Gift"
           orderDescription="Viverra fames massa integer fermentum nisl. Neque morbi cond"
           linkImg="assets/order/order-3.png"
           price="20"
-          quantity="2"
         />
       </div>
 
